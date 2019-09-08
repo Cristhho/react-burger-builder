744b8089e5c1ce42d6923b32e5c1b21b524dd3bd
cristhian ochoa <cristhho@gmail.com>
2019-09-08 16:48:17 -0500

    mobile navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-		<Auxiliary>
-			<Toolbar />
-			<main className={classes.content}>
-				{props.children}
-			</main>
-		</Auxiliary>
-	)
+class Layout extends Component{
 
-export default layout;
+	state = {
+		showSideDrawer: false
+	}
+
+	sideDrawerToggleHandler = () => {
+		this.setState(prevState => {
+			return {showSideDrawer: !prevState.showSideDrawer}
+		})
+	}
+
+	render() {
+		return(
+			<Auxiliary>
+				<Toolbar openDrawer={this.sideDrawerToggleHandler}/>
+				<SideDrawer show={this.state.showSideDrawer} closed={this.sideDrawerToggleHandler} />
+				<main className={classes.content}>
+					{this.props.children}
+				</main>
+			</Auxiliary>
+		)
+	}
+}
+
+export default Layout;
